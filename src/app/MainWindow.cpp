c1743e9f69b886faf0e7e6615748d944fda0c91c
stli <lst92991@gmail.com>
2026-09-04 14:41:17 +0800

    feat(day01): build main window shell

--- src/app/MainWindow.cpp
+++ src/app/MainWindow.cpp
@@ -2,11 +2,20 @@
 
 #include "ui_MainWindow.h"
 
+#include <QAction>
+#include <QPlainTextEdit>
+#include <QStatusBar>
+
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
     , m_ui(new Ui::MainWindow)
 {
     m_ui->setupUi(this);
+
+    connect(m_ui->actionExit, &QAction::triggered, this, &QWidget::close);
+
+    m_ui->activityLog->appendPlainText(tr("Day 1：主窗口外壳已就绪"));
+    statusBar()->showMessage(tr("Day 1：尚未接入设备和插件"));
 }
 
 MainWindow::~MainWindow()
